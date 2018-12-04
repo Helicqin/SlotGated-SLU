06913919f85cd1497a3308caf34ccbc5461e0176
Helicqin <helicbupt@gmail.com>
2018-12-04 10:46:46 +0800

    no big changes

--- unit_test.py
+++ unit_test.py
@@ -1,4 +1,12 @@
 # -*- coding:utf8 -*-
 # Created by Helic on 2018/12/4
+import tensorflow as tf
+print(tf.__version__)
 
+tf.enable_eager_execution()
 
+a = tf.constant(tf.ones([2, 3]))
+b = tf.constant(tf.zeros([2, 3]))
+print(a * b)
+
+print(tf.get_variable('v1', shape=[10]))
